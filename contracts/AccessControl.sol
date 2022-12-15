862c42ec03b0aa9e8098c5054bb343763a818868
Top-Kraken <topkyle521@gmail.com>
2022-12-15 09:31:50 -0500

    update comments

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -9,6 +9,40 @@ import "./interfaces/IAccessControl.sol";
 import "./interfaces/IBunzz.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
+/**
+ * @dev Contract module that allows implementing role-based access control mechanisms.
+ *
+ * Roles are referred to by their `bytes32` identifier. These should be exposed
+ * in the external API and be unique. The best way to achieve this is by
+ * using `public constant` hash digests:
+ *
+ * ```
+ * bytes32 public constant MY_ROLE = keccak256("MY_ROLE");
+ * ```
+ *
+ * Roles can be used to represent a set of permissions. To restrict access to a
+ * function call, use {hasRole}:
+ *
+ * ```
+ * function foo() public {
+ *     require(hasRole(MY_ROLE, msg.sender));
+ *     ...
+ * }
+ * ```
+ *
+ * Roles can be granted and revoked dynamically via the {grantRole} and
+ * {revokeRole} functions. Each role has an associated admin role, and only
+ * accounts that have a role's admin role can call {grantRole} and {revokeRole}.
+ *
+ * By default, the admin role for all roles is `DEFAULT_ADMIN_ROLE`, which means
+ * that only accounts with this role will be able to grant or revoke other
+ * roles. More complex role relationships can be created by using
+ * {_setRoleAdmin}.
+ *
+ * WARNING: The `DEFAULT_ADMIN_ROLE` is also its own admin: it has permission to
+ * grant and revoke this role. Extra precautions should be taken to secure
+ * accounts that have been granted it.
+ */
 contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
     using EnumerableSet for EnumerableSet.AddressSet;
 
@@ -21,10 +55,16 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
 
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE` to `_msgSender()`.
+     */
     constructor() {
         _grantAdminRole(_msgSender());
     }
 
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE` to `_contracts[0]`.
+     */
     function connectToOtherContracts(address[] calldata _contracts)
         public
         override
@@ -33,11 +73,22 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         _grantAdminRole(_contracts[0]);
     }
 
+    /**
+     * @dev Modifier that checks that an account has a specific role. Reverts
+     * with a standardized message including the required role.
+     *
+     * The format of the revert reason is given by the following regular expression:
+     *
+     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
+     */
     modifier onlyRole(bytes32 role) {
         _checkRole(role, _msgSender());
         _;
     }
 
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -50,10 +101,25 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
             super.supportsInterface(interfaceId);
     }
 
+    /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
     function hasRole(bytes32 role, address account) public view returns (bool) {
         return _roles[role].accounts.contains(account);
     }
 
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleGranted} event.
+     */
     function grantRole(bytes32 role, address account)
         public
         onlyRole(getRoleAdmin(role))
@@ -61,6 +127,17 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         _grantRole(role, account);
     }
 
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleRevoked} event.
+     */
     function revokeRole(bytes32 role, address account)
         public
         onlyRole(getRoleAdmin(role))
@@ -68,14 +145,41 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         _revokeRole(role, account);
     }
 
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been revoked `role`, emits a {RoleRenounced}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `account`.
+     *
+     * May emit a {RoleRenounced} event.
+     */
     function renounceRole(bytes32 role, address account) public onlyRole(role) {
         _renounceRole(role, account);
     }
 
+    /**
+     * @dev Returns the admin role that controls `role`. See {grantRole} and
+     * {revokeRole}.
+     *
+     * To change a role's admin, use {_setRoleAdmin}.
+     */
     function getRoleAdmin(bytes32 role) public view returns (bytes32) {
         return _roles[role].adminRole;
     }
 
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
     function setRoleAdmin(bytes32 role, bytes32 adminRole)
         public
         onlyRole(getRoleAdmin(role))
@@ -83,6 +187,16 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         _setRoleAdmin(role, adminRole);
     }
 
+    /**
+     * @dev Returns one of the accounts that have `role`. `index` must be a
+     * value between 0 and {getRoleCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleAt} and {getRoleCount}, make sure
+     * you perform all queries on the same block.
+     */
     function getRoleAt(bytes32 role, uint256 index)
         public
         view
@@ -91,10 +205,31 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         return _roles[role].accounts.at(index);
     }
 
+    /**
+     * @dev Returns the number of accounts that have `role`. Can be used
+     * together with {getRoleAt} to enumerate all bearers of a role.
+     */
     function getRoleCount(bytes32 role) public view returns (uint256) {
         return _roles[role].accounts.length();
     }
 
+    /**
+     * @dev Revert with a standard message if `_msgSender()` is missing `role`.
+     * Overriding this function changes the behavior of the {onlyRole} modifier.
+     *
+     * Format of the revert message is described in {_checkRole}.
+     */
+    function _checkRole(bytes32 role) internal view virtual {
+        _checkRole(role, _msgSender());
+    }
+
+    /**
+     * @dev Revert with a standard message if `account` is missing `role`.
+     *
+     * The format of the revert reason is given by the following regular expression:
+     *
+     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
+     */
     function _checkRole(bytes32 role, address account) internal view {
         if (!hasRole(role, account)) {
             revert(
@@ -110,6 +245,11 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         }
     }
 
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
     function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal {
         bytes32 previousAdminRole = getRoleAdmin(role);
         _roles[role].adminRole = adminRole;
@@ -117,6 +257,13 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         emit AdminRoleChanged(role, previousAdminRole, adminRole);
     }
 
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE` to `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleGranted} event.
+     */
     function _grantAdminRole(address account) internal {
         if (!hasRole(DEFAULT_ADMIN_ROLE, account)) {
             _roles[DEFAULT_ADMIN_ROLE].accounts.add(account);
@@ -125,6 +272,13 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         }
     }
 
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleGranted} event.
+     */
     function _grantRole(bytes32 role, address account) internal {
         require(
             role != DEFAULT_ADMIN_ROLE,
@@ -137,6 +291,13 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         }
     }
 
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleRevoked} event.
+     */
     function _revokeRole(bytes32 role, address account) internal {
         require(
             role != DEFAULT_ADMIN_ROLE,
@@ -149,6 +310,13 @@ contract AccessControl is Context, Ownable, IAccessControl, IBunzz, ERC165 {
         }
     }
 
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleRenounced} event.
+     */
     function _renounceRole(bytes32 role, address account) internal {
         require(
             account == _msgSender(),
